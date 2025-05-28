65a7654ef719b839e23563e91012ca1fcb652d1e
Amal A M <vectorhero007@gmail.com>
2025-05-28 19:51:39 +0530

    row

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,37 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
-              Container(child: Icon(Icons.account_box,),),
+              Card(
+                margin: EdgeInsets.all(15),
+                // padding: EdgeInsets.all(15),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.deepPurple),
+                  title: Text(
+                    '+91 9874563210',
+                    style: TextStyle(fontSize: 20.0, color: Colors.deepPurple),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(15),
+                // padding: EdgeInsets.all(15),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    // Padding(padding: EdgeInsets.fromLTRB(0, 25, 25, 25)),
+                    Icon(Icons.email, color: Colors.deepPurple),
+                    SizedBox(width: 10.0),
+                    Text(
+                      'anaswara123@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -45,3 +75,20 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
+// Row(
+//                   children: [
+//                     // Padding(padding: EdgeInsets.fromLTRB(0, 25, 25, 25)),
+//                     Icon(Icons.email, color: Colors.deepPurple),
+//                     SizedBox(width: 10.0),
+//                     Text(
+//                       'anaswara123@gmail.com',
+//                       style: TextStyle(
+//                         fontSize: 20.0,
+//                         color: Colors.deepPurple,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
